3b23a9c5c0de3065e37194494cda46462ccf481e
Ahmad Wael <ahmadwael2004@gmail.com>
2024-11-25 02:12:55 +0200

    Solved a bug in colorizing line segment

--- Mini_Paint/src/backend/LineSegmentShape.java
+++ Mini_Paint/src/backend/LineSegmentShape.java
@@ -8,6 +8,10 @@ public class LineSegmentShape extends AbstractShape {
     @Override
     public void draw(Graphics canvas) {
         canvas.setColor(super.getColor());
+        if(super.getFillColor() != null) {
+            canvas.setColor(super.getFillColor());
+            canvas.drawLine(super.getPosition().x, super.getPosition().y, super.getProperties().get("x2").intValue(), super.getProperties().get("y2").intValue());
+        }
         canvas.drawLine(super.getPosition().x, super.getPosition().y, super.getProperties().get("x2").intValue(), super.getProperties().get("y2").intValue());
     }
 }
